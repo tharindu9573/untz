f3d72e6ba8faa9a8c776c0f8af1e8ea4cc90cf0b
Tharindu Senevirathna <TharinduS@compello.com>
2023-09-09 13:39:21 +0530

    New implementation

--- UntzClient/src/app/shared/components/home/home.component.ts
+++ UntzClient/src/app/shared/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { UntzEvent } from 'src/app/models/event';
@@ -10,22 +10,34 @@ import { Countdown } from 'src/utility/countdown';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit, AfterViewInit{
   countDownTime?: string = '';
   untzEvent!: UntzEvent;
+  imageVisible: boolean = true;
+  date?: string;
+  month?: string;
+  year?: string;
 
   constructor(private eventService: EventService, private router: Router){
     
   }
+  ngAfterViewInit(): void {
+    var video = document.getElementById("teaser") as HTMLVideoElement;
+        video.onloadeddata = () => {
+          video.play();
+          this.imageVisible = false;  
+        };
+  }
+
   ngOnInit(): void {
 
     this.eventService.getMainEvent().subscribe(_ => {
       if(_)
       {
-        this.untzEvent = _;
+        this.untzEvent = _;      
         Countdown.setCountDown(_.eventStartTime).subscribe(_ => {
           this.countDownTime = _;
-        });
+        });        
       }    
     });
 
